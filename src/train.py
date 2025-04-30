1c53ff994b16328332caeec74edf0b652d9e1a0e
conorosully <conorosullivan@conor.local>
2025-04-30 14:10:07 +0100

    removed old files

---
+++ src/train.py
@@ -0,0 +1,254 @@
+# Training unet for coastline detection
+# Conor O'Sullivan
+# 07 Feb 2023
+
+# Imports
+import numpy as np
+import pandas as pd
+import random
+import glob
+import argparse
+import os
+
+import torch
+import torch.nn as nn
+import torchvision
+from torch.utils.data import DataLoader
+
+from network import U_Net, AttU_Net
+
+import utils
+
+
+def main():
+    # Define the argument parser
+    parser = argparse.ArgumentParser(description="Train a model on specified dataset")
+
+    # Adding arguments
+    parser.add_argument("--model_name", type=str, help="Name of the model to train")
+    parser.add_argument("--sample", action="store_true", help="Whether to use a sample dataset")
+    parser.add_argument("--model_type", type=str, default="UNET", help="Type of model to train")
+    parser.add_argument("--batch_size", type=int, default=32, help="Batch size for training")
+    parser.add_argument("--epochs", type=int, default=50, help="Number of epochs to train for")
+    #parser.add_argument("--loss", type=str, default="BCEWithLogitsLoss", help="Loss function to use")
+    #parser.add_argument("--lr", type=float, default=0.001, help="Learning rate for training")
+    parser.add_argument("--split", type=float, default=0.9, help="Train/Validation split")
+    parser.add_argument("--early_stopping", type=int, default=-1, help="Number of epochs to wait before stopping training. -1 to disable.")
+
+    parser.add_argument("--train_path", type=str, default="../data/training/", help="Path to the training data")
+    parser.add_argument("--save_path", type=str, default="../models/", help="Path template for saving the model")
+    parser.add_argument("--device", type=str, default="cuda", choices=["cuda", "cpu", "mps"], help="Device to use for training")
+    parser.add_argument("--seed", type=int, default=42, help="Random seed for shuffling the dataset")
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Set device based on argument
+    args.device = torch.device(args.device)
+
+    print("\nTraining model with the following arguments:")
+    print(vars(args))  # Print all arguments
+
+    # Load data
+    train_loader, valid_loader = load_data(args)
+    data_sense_check(train_loader)
+
+    # Train the model
+    for loss in ["BCE","wBCE","DICE"]:
+        print(f"\n--- Training with Loss: {loss} ---")
+        args.loss = loss
+        train_model(train_loader, valid_loader, args)
+    
+
+def data_sense_check(loader):
+    """Check the data loader by iterating through it and printing the shapes of the images and targets"""
+
+    print("\nData sense check:")
+    for i, (images, target) in enumerate(loader):
+        print("Batch {}:".format(i))
+        print("Image shape:", images.shape)
+        print("Min:", images.min(), "Max:", images.max())
+        print("Target shape:", target.shape)
+        print("Target unique values:", torch.unique(target))
+
+        if i == 1:
+            break  # Just check the first two batches
+    print()
+
+
+# Classes
+class TrainDataset(torch.utils.data.Dataset):
+    def __init__(self, paths, args):
+        self.paths = paths
+
+    def __getitem__(self, idx):
+        """Get image and binary mask for a given index"""
+
+        path = self.paths[idx]
+        instance = np.load(path)
+
+        # Get spectral bands
+        bands = instance[0:4] # Blue, Green, Red, NIR
+        bands = bands.astype(np.float32) 
+
+        # Normalise bands
+        bands = utils.scale_bands(bands, satellite='sentinel')
+
+        # Convert to tensor
+        bands = torch.tensor(bands)
+
+        # Get target
+        mask = instance[4].astype(np.int8)  # Binary edge mask
+        target = torch.tensor(mask, dtype=torch.float32).unsqueeze(0)
+
+        return bands, target
+
+    def __len__(self):
+        return len(self.paths)
+
+
+# Functions
+def load_data(args):
+    """Load data from disk"""
+
+    # Correct path format
+    if args.train_path[-1] != "/":
+        args.train_path += "/"
+
+    paths = glob.glob(args.train_path + "*.npy")
+    print("\nTotal images: {}".format(len(paths)))
+
+    if args.sample:
+        paths = paths[:100]
+
+    # Shuffle the paths
+    random.seed(args.seed)
+    random.shuffle(paths)
+
+    # Create datasets
+    split = int(args.split * len(paths))
+    train_data = TrainDataset(paths[:split], args)
+    valid_data = TrainDataset(paths[split:], args)
+
+    # Prepare data for PyTorch model
+    train_loader = DataLoader(train_data, batch_size=args.batch_size, shuffle=True)
+    valid_loader = DataLoader(valid_data, batch_size=args.batch_size)
+
+    return train_loader, valid_loader
+
+class DiceLoss(nn.Module):
+    """Dice Loss for binary segmentation with sigmoid"""
+    def __init__(self, smooth=1e-6):
+        super().__init__()
+        self.smooth = smooth
+
+    def forward(self, inputs, targets):
+        inputs = torch.sigmoid(inputs)
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        intersection = (inputs * targets).sum()
+        return 1 - (2. * intersection + self.smooth) / (inputs.sum() + targets.sum() + self.smooth)
+
+
+def initialize_model(args, lr):
+    """Initialize the model, optimizer and criterion based on the specified model type"""
+
+    out_channels = 1
+    n_bands = 4
+
+    if args.model_type == "UNET":
+        model = U_Net(n_bands, out_channels)
+    elif args.model_type == "ATTUNET":
+        model = AttU_Net(n_bands, out_channels)
+    else:
+        raise ValueError("Unsupported model type")
+
+    model.to(args.device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+   
+    if args.loss == "BCE":
+        criterion = nn.BCEWithLogitsLoss()
+
+    elif args.loss == "wBCE":
+        pos_weight = 183 # weight given to edge pixels
+        pos_weight = torch.tensor(pos_weight, dtype=torch.float32).to(args.device)
+        criterion = nn.BCEWithLogitsLoss(pos_weight=pos_weight)
+
+    elif args.loss == "DICE":
+        criterion = DiceLoss()
+
+    return model, optimizer, criterion
+
+
+def train_one_epoch(model, train_loader, criterion, optimizer, device):
+    """Train the model for one epoch"""
+
+    model.train()
+    for images, target in train_loader:
+        images = images.to(device)
+        target = target.to(device).float()
+
+        optimizer.zero_grad()
+        output = model(images)
+
+        loss = criterion(output, target)
+        loss.backward()
+        optimizer.step()
+
+
+def evaluate_model(model, valid_loader, criterion, device):
+    """Evaluate the model on the validation set"""
+    model.eval()
+    valid_loss = 0.0
+    with torch.no_grad():
+        for images, target in valid_loader:
+            images = images.to(device)
+            target = target.to(device).float()
+            output = model(images)
+            valid_loss += criterion(output, target).item()
+
+    return valid_loss / len(valid_loader)
+
+
+def train_model(train_loader, valid_loader, args):
+    """Train the model with different learning rates and save the best model"""
+
+    learning_rates = [0.1, 0.01, 0.001]
+    global_min_loss = np.inf # Compare across all learning rates
+
+    for lr in learning_rates:
+        print(f"\n--- Training with Learning Rate: {lr} ---")
+
+        model, optimizer, criterion = initialize_model(args, lr)
+
+        min_loss = np.inf # Compare within this learning rate
+        epochs_no_improve = 0 # Early stopping counter
+        for epoch in range(args.epochs):
+            print(f"Epoch {epoch + 1} | ", end=" ")
+
+            train_one_epoch(model, train_loader, criterion, optimizer, args.device)
+            valid_loss = evaluate_model(model, valid_loader, criterion, args.device)
+
+            print(f"Validation Loss: {valid_loss:.5f}")
+
+            if valid_loss < global_min_loss:
+                save_path = os.path.join(args.save_path, f"{args.model_name}_{args.model_type}_{args.loss}.pth")
+                torch.save(model.state_dict(), save_path)
+                print("Model saved (best across all LRs).")
+                global_min_loss = valid_loss
+
+            # Update early stopping counter
+            if valid_loss < min_loss:
+                min_loss = valid_loss
+                epochs_no_improve = 0
+            else:
+                epochs_no_improve += 1
+
+            if args.early_stopping > 0 and epochs_no_improve >= args.early_stopping:
+                print("Early stopping triggered.")
+                break
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/utils.py
@@ -0,0 +1,234 @@
+# Util functions used to create SICS dataset
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import os
+import glob
+
+import rasterio
+from rasterio.enums import Resampling
+from rasterio.windows import from_bounds
+
+import cv2
+
+import spyndex
+import xarray as xr
+
+# Global variables
+channels = ['Blue','Green','Red','NIR']
+
+standards = ['B','G','R','N']
+
+def get_index(bands, index):
+
+        """Calculate spectral index using spyndex library"""
+
+        # scale bands to 0-1
+        bands = scale_bands(bands)
+
+        da = xr.DataArray(
+            bands,
+            dims = ("band","x","y"),
+            coords = {"band": channels}
+        )
+
+        params = {standard: da.sel(band = channel) for standard,channel in zip(standards,channels)}
+
+        idx = spyndex.computeIndex(
+            index = index,
+            params = params)
+
+
+        return idx
+
+def stack_10m_bands(safe_dir):
+    """Stacks Sentinel-2 10m bands from a given .SAFE directory.
+    Returns:
+        stacked_array (numpy.ndarray): Stacked Sentinel-2 bands (Bands x Height x Width)
+        reference_raster (rasterio.DatasetReader): Rasterio dataset for metadata access
+    """
+    
+    # Define band selection (Sentinel-2 L2A band resolutions)
+    band_map = {
+        "B02": "R10m",  # Blue
+        "B03": "R10m",  # Green
+        "B04": "R10m",  # Red
+        "B08": "R10m"   # NIR
+    }
+
+    granule_path = glob.glob(os.path.join(safe_dir, "GRANULE", "*"))[0]  # Get the GRANULE directory
+    img_data_path = os.path.join(granule_path, "IMG_DATA")  # IMG_DATA directory
+
+    stacked_bands = []
+    reference_raster = None  # To store a rasterio dataset
+
+    for band, res in band_map.items():
+        band_path = glob.glob(os.path.join(img_data_path, res, f"*_{band}_*.jp2"))
+        if not band_path:
+            print(f"Band {band} not found, skipping...")
+            continue
+        
+        band_path = band_path[0]
+        with rasterio.open(band_path) as src:
+            band_data = src.read(1)
+
+            stacked_bands.append(band_data)
+
+            # Use the first valid raster file as a reference
+            if reference_raster is None:
+                reference_raster = rasterio.open(band_path)
+
+    if not stacked_bands:
+        raise ValueError("No valid Sentinel-2 bands found in the given SAFE directory.")
+
+    # Stack into a single array (Bands x Height x Width)
+    stacked_array = np.stack(stacked_bands, axis=0)
+    return stacked_array, reference_raster
+
+def stack_crop_resample_index(safe_dir, output_tif, utm=None,resampling_factor=1):
+    """Stacks Sentinel-2 10m bands, crops using UTM coordinates, resamples, 
+    adds NDWI and NDVI and and saves as a .tif.
+    
+    Args:
+        safe_dir (str): Path to the Sentinel-2 .SAFE directory.
+        output_tif (str): Path to save the output .tif file.
+        resampling_factor (float): Resampling factor (default=1, no resampling).
+        p1 (tuple): First UTM coordinate (x_min, y_min).
+        p2 (tuple): Second UTM coordinate (x_max, y_max).
+
+    Returns:
+        None
+    """
+    
+    band_map = {
+        "B02": "R10m",  # Blue
+        "B03": "R10m",  # Green
+        "B04": "R10m",  # Red
+        "B08": "R10m"   # NIR
+    }
+
+    granule_path = glob.glob(os.path.join(safe_dir, "GRANULE", "*"))[0]
+    img_data_path = os.path.join(granule_path, "IMG_DATA")
+
+    stacked_bands = []
+    reference_raster = None
+    left, top, right, bottom = utm
+
+    for band, res in band_map.items():
+        band_path = glob.glob(os.path.join(img_data_path, res, f"*_{band}_*.jp2"))
+        if not band_path:
+            print(f"Band {band} not found, skipping...")
+            continue
+        
+        band_path = band_path[0]
+
+        with rasterio.open(band_path) as src:
+
+            # Define cropping window
+            window = from_bounds(left, bottom, right, top, transform=src.transform)
+
+            # New dimensions after resampling
+            height = int(window.height * resampling_factor)
+            width = int(window.width * resampling_factor)
+
+            band_data = src.read(1,
+                                window=window,
+                                out_shape=(1, height, width),
+                                resampling=Resampling.bilinear)
+     
+
+            # Compute the transform for the cropped image first
+            transform = src.window_transform(window)
+
+            # Apply resampling scaling to the transform
+            transform = transform * transform.scale(
+                (window.width / width),
+                (window.height / height)
+            )
+
+            stacked_bands.append(band_data)
+
+            # Store reference raster metadata
+            if reference_raster is None:
+                reference_raster = src
+    
+
+    if not stacked_bands:
+        raise ValueError("No valid Sentinel-2 bands found.")
+
+    stacked_array = np.stack(stacked_bands, axis=0)
+
+    # Calculate NDWI and NDVI
+    NDWI = get_index(stacked_array, 'NDWI')
+    NDVI = get_index(stacked_array, 'NDVI')
+    NDWI = NDWI.expand_dims(dim="band")
+    NDVI = NDVI.expand_dims(dim="band")
+
+    stacked_array = np.vstack((stacked_array, NDWI, NDVI))
+
+    # Save output .tif file
+    out_meta = reference_raster.meta.copy()
+    out_meta.update({
+        "driver": "GTiff",
+        "height": stacked_array.shape[1],
+        "width": stacked_array.shape[2],
+        "count": stacked_array.shape[0],
+        "dtype": stacked_array.dtype,
+        "transform": transform
+    })
+
+    with rasterio.open(output_tif, "w", **out_meta) as dst:
+        for i in range(stacked_array.shape[0]):
+            dst.write(stacked_array[i], i + 1)
+
+    print(f"Saved stacked Sentinel-2 bands to {output_tif}")
+
+    return stacked_array
+
+def display_bands(img, downsample=True,scale=10):
+    """Visualize all SR bands of a satellite image."""
+
+    band_names = ["Blue", "Green", "Red", "NIR"]
+    n = len(band_names)
+
+    fig, axs = plt.subplots(1, n, figsize=(20, 5))
+
+    for i in range(n):
+        band = img[i]
+        if downsample:
+            band = cv2.resize(band, (band.shape[1] // scale, band.shape[0] // scale), interpolation=cv2.INTER_AREA)
+        axs[i].imshow(band, cmap="gray")
+        axs[i].set_title(band_names[i])
+        axs[i].axis("off")
+
+def scale_bands(img,satellite="sentinel"):
+    """Scale bands to 0-1"""
+    img = img.astype("float32")
+    if satellite == "landsat":
+        img = np.clip(img * 0.0000275 - 0.2, 0, 1)
+    elif satellite == "sentinel":
+        img = np.clip(img/10000, 0, 1)
+    return img
+
+def get_rgb(img, r=2,g=1,b=0, contrast=1):
+    """Convert a stacked array of bands to RGB"""
+
+    if img.shape[0] > img.shape[2]:
+        #  H x W x B -> B x H x W
+        img = np.transpose(img, (2,0,1))
+
+    r = img[r]
+    g = img[g]
+    b = img[b]
+
+    rgb = np.stack([r, g, b], axis=-1)
+    rgb = rgb.astype(np.float32)
+
+    rgb = scale_bands(rgb)
+    rgb = np.clip(rgb, 0, contrast) / contrast
+
+    # convert to 255
+    rgb = (rgb * 255).astype(np.uint8) 
+
+    return rgb
